034edc3955d27a2b611e834fc26ac7d0ee07fdd5
lalarukhh <lalaarukhhh@gmail.com>
2023-06-21 10:29:41 +0500

    ALLAH plz heal my heart Ameen!

--- Doctor/urls.py
+++ Doctor/urls.py
@@ -9,7 +9,7 @@ from MyProject import settings
 from . import views
 urlpatterns=[
      path('admin/', admin.site.urls),
-     path('home/',views.Home,name="Homepage"),
+     path('',views.Home,name="Homepage"),
 
      path('home/prescription/',views.INDEX,name="main_prescription"),
      path('prescription/',views.INDEX,name="main_prescription"),
